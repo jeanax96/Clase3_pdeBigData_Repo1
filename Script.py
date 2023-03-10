2cd3b626d14ba053487de46f5961dbbb0b3a329b
JeanCamposPC <jean.ax96@gmail.com>
2023-03-09 20:38:14 -0500

    Se ha agregado algunas lineas en el archivo Script.py luego de configurar nuestrto entorno de trabajo (Anaconda Prompt).

--- Script.py
+++ Script.py
@@ -1,6 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Autor: Jean Carlos Campos
+"""
+# %% Cargamos modulos y datos
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sympy
+import scipy
+
+# Datos
+
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+
+
 
 # Agregamos un conjunto de datos para realizar una regresion logistica multiclase
